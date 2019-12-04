01d1ca02b32084832484d13690f0454934cf4bca
Daniel <32192253+bikerbent@users.noreply.github.com>
2019-12-04 09:55:49 +0100

    k

---
+++ yeetles_13/yeetles_13/Database.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace yeetlse_13
+{
+    public class Database
+    {
+        public static string ConnectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=HovedOpgave;Integrated Security=True;";
+
+        public static bool Login(string userName, string password)
+        {
+            bool verification = false;
+            using (SqlConnection conn = new SqlConnection(ConnectionString))
+            {
+                try
+                {
+                    SqlCommand command = new SqlCommand($"SELECT ID FROM UserTable WHERE UserName = '{userName}' AND Password = '{password}'", conn);
+                    command.Connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+                    if (reader.Read())
+                    {
+                        verification = true;
+                    }
+                    command.Connection.Close();
+                }
+                catch (Exception e)
+                {
+                }
+                return verification;
+            }
+        }
+    }
+}
---
+++ yeetles_13/yeetles_13/Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace yeetlse_13
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void LoginButton_Click(object sender, EventArgs e)
+        {
+            if(Database.Login(UserNameTextBox.Text, PasswordTextBox.Text))
+            {
+                Session["CurrentUser"] = UserNameTextBox.Text;
+                Response.Redirect("Contact.aspx");
+            }
+        }
+    }
+}
